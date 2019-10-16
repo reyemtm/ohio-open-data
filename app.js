7fde1739107195dae00a3f065cec6ffcd6ab6971
Malcolm Meyer <malcolmtmeyer@gmail.com>
2019-10-16 18:42:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -26,3 +26,44 @@ var map = new mapboxgl.Map({
 });
 map.addControl(new mapboxgl.NavigationControl());
 map.addControl(new mapboxgl.FullscreenControl());
+
+map.on('style.load', function() {
+  
+  map.addSource('counties', {
+    'type': 'geojson',
+    'data': 'https://ovrdc.github.io/gis-tutorials/tutorial-data/counties.geojson'
+  });
+
+  map.addLayer({
+    'id': 'countiesFill',
+    'type': 'fill',
+    'source': 'counties',
+    'layout': {
+      'visibility': 'visible'
+    },
+    'paint': {
+      'fill-color': 'transparent',
+      'fill-outline-color': 'white',
+      'fill-opacity': 0.9
+    }
+  });
+  
+  map.addLayer({
+    'id': 'countiesLine',
+    'type': 'line',
+    'source': 'counties',
+    'layout': {
+      'visibility': 'visible'
+    },
+    'paint': {
+      'line-color': "#121212",
+      'line-width': 3
+    }
+  });
+});
+
+map.on("click", mapQuery)
+
+function mapQuery() {
+  console.log(map.queryRenderedFeatures(this.point))
+}
