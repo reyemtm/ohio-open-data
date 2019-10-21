80b2d9755f67b7a70baa2de15165bcd5af144a80
malcolm <malcolmtmeyer@gmail.com>
2019-10-20 21:26:12 -0400

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function initMap() {
 
   var map = new mapboxgl.Map({
     container: 'map',
-    hash: true,
+    hash: false,
     /*style: 'some mapbox style url*/
     /*below is a blank style*/
     style: {
@@ -108,7 +108,7 @@ function initMap() {
       },
       'paint': {
         'fill-color': ['case', 
-        ["!=", ["length", ['get', 'auditor_map_link']], 0], '#FFEB3B', 
+        ["!=", ["length", ['get', 'gis_link']], 0], '#FFEB3B', 
         'lightgray'],
         'fill-outline-color': '#212121',
         'fill-opacity': 0.9
@@ -129,16 +129,17 @@ function initMap() {
     });
 
 
-    fetch("https://reyemtm.github.io/ohio-open-data/ohio.geojson")
+    fetch("ohio.geojson")
       .then(res => {
         return res.json()
       })
       .then(json => {
         var jsonJoined = joinData(json, joinKeys)
+        console.log(jsonJoined)
         map.getSource("counties").setData(jsonJoined)
       });
 
-    fetch("https://reyemtm.github.io/ohio-open-data/places.geojson")
+    fetch("places.geojson")
       .then(res => {
         return res.json()
       })
@@ -147,11 +148,23 @@ function initMap() {
         map.getSource("places").setData(jsonJoined)
       });
 
+      map.addControl(new selectControl(), "top-left");
+
+      map.addControl(new MapboxGLButtonControl({
+        className: "mapbox-gl-draw_line",
+        title: "Draw Line",
+        eventHandler: toggleTable
+      }));
+
   });
 
-  map.on("click", mapQuery)
+  var popup = new mapboxgl.Popup();
+
+  map.on("click", mapQuery);
+  map.on("mousemove", "placesFill", mapQuery)
 
   function mapQuery(e) {
+    popup.remove();
     var point = e;
     var features = getFeatures(point);
     console.log(features)
@@ -163,7 +176,7 @@ function initMap() {
       if ((features[0].properties.name).includes("County")) {
         url = "https://duckduckgo.com/?q=" + features[0].properties.name + "ohio+auditor"
       }
-      new mapboxgl.Popup()
+      popup
         .setLngLat(e.lngLat)
         .setHTML(`<h3>${features[0].properties.name}</h3><a href='${url}' target='_blank'>Search for Auditor Site</a><br>${html}`)
         .addTo(map);
@@ -221,7 +234,8 @@ function getPopupHtml(properties) {
     var key = p.replace(/_/g, " ");
     // key = key.toLocaleUpperCase()
     var value = properties[p];
-    if (properties[p].includes("http")) value = `<a href='${properties[p]}' target='_blank'>Link</a>`;
+    console.log(value)
+    if (properties[p].toString().includes("http")) value = `<a href='${properties[p]}' target='_blank'>Link</a>`;
     console.log(properties[p])
     if (properties[p]) html += `<strong>${key}</strong>: ${value}<br>`
   }
@@ -229,11 +243,95 @@ function getPopupHtml(properties) {
 }
 
 function joinData(geojson, data) {
-  geojson.features.map(f => {
+  geojson.features.map(function(f,i) {
     var key = (!f.properties.GEOID) ? 0 : f.properties.GEOID;
     if (key && data[key]) {
-      f.properties = data[key]
+      f.properties = {...f.properties, ...data[key]}
     }
   })
   return geojson
+}
+
+class selectControl {
+  // constructor({ bearing = -20, pitch = 70, minpitchzoom = null }) {
+  //   this._bearing = bearing;
+  //   this._pitch = pitch;
+  //   this._minpitchzoom = minpitchzoom;
+  // }
+
+  onAdd(map) {
+    this._map = map;
+    let _this = this;
+
+    this._btn = document.createElement("button");
+    this._btn.className = "";
+    this._btn.type = "button";
+    this._btn["aria-label"] = "Select Map Theme";
+    this._btn.style.minWidth = "100px";
+    this._form = document.createElement("form")
+    // this._form.innerHTML = "<label for='select'>Map Theme</label>"
+    this._select = document.createElement("select");
+    this._select.id = "select";
+    this._select.innerHTML = "<option value='1'>One</option><option value='2'>Two</option>"
+    this._select.onchange = function(e) {
+      console.log(e.target.value);
+      console.log(map)
+    }
+    this._form.appendChild(this._select);
+    this._btn.appendChild(this._form)
+    this._container = document.createElement("div");
+    this._container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+    this._container.appendChild(this._btn);
+
+    return this._container;
+  }
+
+  onRemove() {
+    this._container.parentNode.removeChild(this._container);
+    this._map = undefined;
+  }
+}
+
+
+class MapboxGLButtonControl {
+  constructor({
+    className = "",
+    title = "",
+    eventHandler = evtHndlr
+  }) {
+    this._className = className;
+    this._title = title;
+    this._eventHandler = eventHandler;
+  }
+
+  onAdd(map) {
+    this._btn = document.createElement("button");
+    this._btn.className = "mapboxgl-ctrl-icon" + " " + this._className;
+    this._btn.type = "button";
+    this._btn.title = this._title;
+    this._btn.onclick = this._eventHandler;
+
+    this._container = document.createElement("div");
+    this._container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+    this._container.appendChild(this._btn);
+
+    return this._container;
+  }
+
+  onRemove() {
+    this._container.parentNode.removeChild(this._container);
+    this._map = undefined;
+  }
+}
+
+
+function toggleTable(event) {
+  // alert("Event handler when clicking on \r\n" + event.target.className);
+  var table = document.getElementById("table");
+  console.log(table.style.left)
+  if (!table.style.left || table.style.left === "-800px") {
+    table.style.left = "0"
+  }else{
+    table.style.left = "-800px"
+  }
 }
